1835c033e025a56d221d5aaa9044a70b37c0a34f
Evgenii Latyshev <e.latishev@gmail.com>
2019-11-24 13:19:42 +0300

    Fix task 6

--- 6_3.cpp
+++ 6_3.cpp
@@ -1,3 +1,12 @@
+// Латышев Евгений
+// https://contest.yandex.ru/contest/15368/run-report/25965108/
+// Задача 6.3
+// Дано число N ≤ 104 и последовательность целых чисел из [-231..231] длиной N. Требуется построить бинарное дерево,
+// заданное наивным порядком вставки. Т.е., при добавлении очередного числа K в дерево с корнем root, если root→Key ≤ K,
+// то узел K добавляется в правое поддерево root; иначе в левое поддерево root.
+// Выведите элементы в порядке post-order (снизу вверх).
+// Рекурсия запрещена.
+
 #include <iostream>
 #include <stack>
 
@@ -12,45 +21,101 @@ struct TreeNode {
 // Бинарное дерево
 class BinaryTree {
 public:
-    ~BinaryTree();
-    void add(int value);
-    void print() const;
+    explicit BinaryTree() = default;
+    ~BinaryTree() {PostOrder(deleteNode);};
+    BinaryTree(const BinaryTree& other);
+    BinaryTree(BinaryTree&& other) noexcept;
+    BinaryTree& operator=(const BinaryTree& other) = delete;
+    BinaryTree& operator=(BinaryTree&& other) noexcept = delete;
+    void Add(int value);
+    void PrintTree() const {PostOrder(printNode);};
 private:
     TreeNode* root = nullptr;
+    static void deleteNode(TreeNode* currNode) {delete currNode;};
+    static void printNode(TreeNode* currNode) {std::cout << currNode->value << " ";};
+    void PostOrder(const std::function<void(TreeNode*)>& operation) const;
+    void PreOrder(const std::function<void(TreeNode*)>& operation) const;
 };
 
-BinaryTree::~BinaryTree() {
-    std::stack<TreeNode*> nodes_to_print;
-    if (root->right != nullptr) {
-        nodes_to_print.push(root->right);
+// глубокая копия дерева
+BinaryTree::BinaryTree(const BinaryTree& other) {
+    std::stack<TreeNode*> nodesToCopy;
+    std::stack<TreeNode*> nodesToCopyNew;
+    root = new TreeNode(other.root->value);
+    nodesToCopy.push(other.root);
+    nodesToCopyNew.push(root);
+    while (!nodesToCopy.empty()) {
+        TreeNode* currNode = nodesToCopy.top();
+        TreeNode* currNodeNew = nodesToCopyNew.top();
+        nodesToCopy.pop();
+        nodesToCopyNew.pop();
+        if (currNode->right != nullptr) {
+            currNodeNew->right = new TreeNode(currNode->right->value);
+            nodesToCopy.push(currNode->right);
+            nodesToCopyNew.push(currNodeNew->right);
+        }
+        if (currNode->left != nullptr) {
+            currNodeNew->left = new TreeNode(currNode->left->value);
+            nodesToCopy.push(currNode->left);
+            nodesToCopyNew.push(currNodeNew->left);
+        }
     }
-    nodes_to_print.push(root);
+}
+
+BinaryTree::BinaryTree(BinaryTree&& other) noexcept {
+    root = other.root;
+}
+
+// обход дерева сверху вниз
+void BinaryTree::PreOrder(const std::function<void(TreeNode*)>& operation) const {
+    std::stack<TreeNode*> nodesToVisit;
+    nodesToVisit.push(root);
+    while (!nodesToVisit.empty()) {
+        TreeNode* currNode = nodesToVisit.top();
+        operation(currNode);
+        nodesToVisit.pop();
+        if (currNode->right != nullptr) {
+            nodesToVisit.push(currNode->right);
+        }
+        if (currNode->left != nullptr) {
+            nodesToVisit.push(currNode->left);
+        }
+    }
+}
+
+// обход дерева снизу вверх
+void BinaryTree::PostOrder(const std::function<void(TreeNode*)>& operation) const {
+    std::stack<TreeNode*> nodesToVisit;
+    if (root->right != nullptr) {
+        nodesToVisit.push(root->right);
+    }
+    nodesToVisit.push(root);
     TreeNode* currNode = root->left;
     // всегда идём до упора налево, положив в стек правого сына и текущий узел
-    while (!nodes_to_print.empty()) {
+    while (!nodesToVisit.empty()) {
         while (currNode != nullptr) {
             if (currNode->right != nullptr) {
-                nodes_to_print.push(currNode->right);
+                nodesToVisit.push(currNode->right);
             }
-            nodes_to_print.push(currNode);
+            nodesToVisit.push(currNode);
             currNode = currNode->left;
         }
         // когда налево дошли до упора, идём направо
-        currNode = nodes_to_print.top();
-        nodes_to_print.pop();
+        currNode = nodesToVisit.top();
+        nodesToVisit.pop();
         // если есть куда идти направо - спускаемся, иначе - удаляем текущий узел
-        if (currNode->right != nullptr and !nodes_to_print.empty() and currNode->right == nodes_to_print.top()) {
-            nodes_to_print.pop();
-            nodes_to_print.push(currNode);
+        if (currNode->right != nullptr and !nodesToVisit.empty() and currNode->right == nodesToVisit.top()) {
+            nodesToVisit.pop();
+            nodesToVisit.push(currNode);
             currNode = currNode->right;
         } else {
-            delete currNode;
+            operation(currNode);
             currNode = nullptr;
         }
     }
 }
 
-void BinaryTree::add(int value) {
+void BinaryTree::Add(int value) {
     TreeNode* newNode = nullptr;
     if (root == nullptr) {
         root = new TreeNode(value);
@@ -77,37 +142,6 @@ void BinaryTree::add(int value) {
     }
 }
 
-void BinaryTree::print() const {
-    std::stack<TreeNode*> nodes_to_print;
-    if (root->right != nullptr) {
-        nodes_to_print.push(root->right);
-    }
-    nodes_to_print.push(root);
-    TreeNode* currNode = root->left;
-    // всегда идём до упора налево, положив в стек правого сына и текущий узел
-    while (!nodes_to_print.empty()) {
-        while (currNode != nullptr) {
-            if (currNode->right != nullptr) {
-                nodes_to_print.push(currNode->right);
-            }
-            nodes_to_print.push(currNode);
-            currNode = currNode->left;
-        }
-        // когда налево дошли до упора, идём направо
-        currNode = nodes_to_print.top();
-        nodes_to_print.pop();
-        // если есть куда идти направо - спускаемся, иначе - выводим текущий узел
-        if (currNode->right != nullptr and !nodes_to_print.empty() and currNode->right == nodes_to_print.top()) {
-            nodes_to_print.pop();
-            nodes_to_print.push(currNode);
-            currNode = currNode->right;
-        } else {
-            std::cout << currNode->value << " ";
-            currNode = nullptr;
-        }
-    }
-}
-
 int main() {
     int element_count = 0;
     std::cin >> element_count;
@@ -116,8 +150,8 @@ int main() {
     for (int i = 0; i < element_count; i++) {
         int element = 0;
         std::cin >> element;
-        tree.add(element);
+        tree.Add(element);
     }
-    tree.print();
+    tree.PrintTree();
     return 0;
 }
