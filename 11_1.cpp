80c09d80147c15b9ca46e3896ace9324741d8d44
Evgenii Latyshev <e.latishev@gmail.com>
2019-12-18 23:16:08 +0300

    Add comments

--- 11_1.cpp
+++ 11_1.cpp
@@ -1,5 +1,5 @@
 // Латышев Евгений. Задача 11. Вариант 1
-//
+// https://contest.yandex.ru/contest/16167/run-report/29015257/
 // Дан невзвешенный неориентированный граф. Найдите цикл минимальной длины.
 // Ввод: v: кол-во вершин (макс. 50000), n: кол-во ребер (макс. 200000), n пар реберных вершин.
 // Вывод: одно целое число равное длине минимального цикла. Если цикла нет, то вывести -1.
@@ -16,13 +16,13 @@ public:
     ShortestCycleFinder(ShortestCycleFinder&& other) = delete;
     ShortestCycleFinder& operator=(const ShortestCycleFinder& other) = delete;
     ShortestCycleFinder& operator=(ShortestCycleFinder&& other) = delete;
-    int FindShortestCycleLen();
-    void AddEdge(int start, int end);
+
+    int FindShortestCycleLen(); // Поиск цикла минимальной длины
+    void AddEdge(int start, int end); // Добавление ребра в граф
 private:
     int v; // количество вершин
     int n; // количество рёбер
-    std::vector<int> *edges;  // массив список рёбер, исходящих из каждой вершины
-//    int FindShortestCycleVertice(int start, std::vector<bool> &visited);
+    std::vector<int> *edges; // массив списков рёбер, исходящих из каждой вершины
 };
 
 void ShortestCycleFinder::AddEdge(int start, int end) {
@@ -33,20 +33,21 @@ void ShortestCycleFinder::AddEdge(int start, int end) {
 int ShortestCycleFinder::FindShortestCycleLen() {
     int minLen = n+2;  // Недостижимая максимальная длина цикла в графе
     for (int i = 0; i < v; i++) {
-        std::vector<int> distance(v, (int)(n+2));
+        std::vector<int> distance(v, (int)(n+2)); // расстояния от текущей вершины до каждой вершины графа
         distance[i] = 0;
-        std::vector<int> parent(v, -1);
+        std::vector<int> parent(v, -1);  // храним, откуда мы пришли в вершину
         std::queue<int> nodesToVisit;
         nodesToVisit.push(i);
-        while (!nodesToVisit.empty()) {
+        while (!nodesToVisit.empty()) { // идём по всем достижимым узлам
             int currNode = nodesToVisit.front();
             nodesToVisit.pop();
-            for (int adj : edges[currNode]) {
+            for (int adj : edges[currNode]) { // если еще не заходили в смежную с текущей вершину, добавляем её в очередь
                 if (distance[adj] == (int)(n+2)) {
                     distance[adj] = 1 + distance[currNode];
                     parent[adj] = currNode;
                     nodesToVisit.push(adj);
                 }
+                // если мы уже были в смежной вершине и не тем путём, которым идем сейчас, мы нашли цикл
                 else if (parent[currNode] != adj and parent[adj] != currNode) {
                     minLen = std::min(minLen, distance[currNode] + distance[adj] + 1);
                 }
